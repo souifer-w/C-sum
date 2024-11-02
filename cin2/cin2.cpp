8e9b25092e115fb7948881fb1fc5f2854810de46
HP <HP@DESKTOP-NMBILJD>
2024-11-02 19:09:07 +0100

    Add project files.

---
+++ cin2/cin2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int  a =20, b =20 , c = 40 ;
+    int sum = a+b+c;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << "________________" << endl;
+    
+    cout << sum << endl;
+    return 0;
+
+}
